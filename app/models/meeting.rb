676e05f4948a992a84ca7f91883a420d891185c9
Monica Mow <mow.monica@gmail.com>
2016-07-09 16:33:32 -0700

    Add validations to Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,12 @@
 class Meeting < ActiveRecord::Base
+
+  validates :meeting_date,
+    presence: true
+
+  validates :title,
+    presence: true 
+
+  validates :status,
+    presence: true   
+
 end
