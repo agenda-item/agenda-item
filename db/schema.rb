9423647c5d0d45aa6b281f5edd253b90f89b791e
Monica Mow <mow.monica@gmail.com>
2016-07-09 15:34:49 -0700

    Add Organizations to the app
    - create Organization model
    - create AddOrganizations migration
    - run migration to create organizations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709204617) do
+ActiveRecord::Schema.define(version: 20160709204632) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "address"
+    t.string   "website"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "organization_id"
