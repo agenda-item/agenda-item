198459514fcbf000b870dbe27dbf5af71eb96bc8
Monica Mow <mow.monica@gmail.com>
2016-07-10 12:55:50 -0700

    Setup User and AgendaItem associations AND add bcrypyt gem
    - AgendaItem can have a responsible_user, mover, or seconder and so it makes three separate references to the User model
    - required creating three separate references with different id names/foreign keys:
      + responsible_user_id
      + mover_id
      + seconder_id
    - on User model, make three has_many associations
    - on AgendaItem model, make three belongs_to associations
    
    - also added bcrypt gem so I can actually create a user without throwing an error b/c of has_secure_password method

--- app/models/agenda_item.rb
+++ app/models/agenda_item.rb
@@ -1,2 +1,10 @@
 class AgendaItem < ActiveRecord::Base  
+  belongs_to :responsible_user, 
+              class_name: "User"
+
+  belongs_to :mover, 
+              class_name: "User"
+              
+  belongs_to :seconder, 
+              class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ActiveRecord::Base
   has_many :meeting_permissions
   has_many :meetings, through: :meeting_permissions
 
+  has_many :responsible_user_agenda_items, 
+            class_name: "AgendaItem", 
+            foreign_key: "responsible_user_id"
+
+  has_many :mover_agenda_items, 
+            class_name: "AgendaItem", 
+            foreign_key: "mover_id"
+
+  has_many :seconder_agenda_items, 
+            class_name: "AgendaItem", 
+            foreign_key: "seconder_id"
+
   has_secure_password
 
   validates :first_name,
