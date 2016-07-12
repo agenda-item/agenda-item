33a9cff02f2f6ad11c3208141b4aeeab5b7cbb6c
Jen <jen@reiher.ca>
2016-07-12 06:33:32 +0000

    display Meeting.all data

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,19 @@ end
 get '/styleguide' do
   erb :styleguide
 end
+
+get '/meetings' do
+  
+
+  erb :meetings
+end
+
+get '/api/meetings' do
+ content_type :json
+ Meeting.all.to_json
+end
+
+get '/api/agenda-items' do
+ content_type :json
+ AgendaItem.all.to_json
+end
