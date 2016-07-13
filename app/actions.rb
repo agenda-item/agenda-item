2f54090842f233177a225f1a9aed2e2329d9f870
Jen <jen@reiher.ca>
2016-07-13 22:13:10 +0000

    votes displaying with user names

--- app/actions.rb
+++ app/actions.rb
@@ -18,15 +18,20 @@ end
 
 get '/api/agenda-items' do
  content_type :json
- AgendaItem.all.to_json
+ AgendaItem.all.to_json(include: :votes)
 end
 
-# get '/api/users/:id' do
-#   content_type :json
-#   User.find(params[:id]).to_json
-# end
+get '/api/users' do
+  content_type :json
+  User.all.to_json
+end
+
+get '/api/users/:id' do
+  content_type :json
+  User.find(params[:id]).to_json
+end
 
 get '/api/votes' do
  content_type :json
- Vote.all.to_json
+ Vote.all.to_json(include: :voting_user)
 end
