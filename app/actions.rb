ff4259e73a11729a4a7a332ad45790c0831317d8
Monica Mow <mow.monica@gmail.com>
2016-07-14 16:45:21 -0700

    Add a signup page/route when user creates new organization

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,13 @@
-# Homepage (Root path)
+# Landing Page
 get '/' do
   erb :index
 end
 
+# Style Guide
 get '/styleguide' do
   erb :styleguide
+end
+
+get '/organizations/new' do
+  erb :signup
 end
