ab7ef2cec40bbe5378d0268f5fd83d0ae69c3270
Monica Mow <mow.monica@gmail.com>
2016-07-14 16:52:02 -0700

    Setup view/routes to list/grab data of organizations
    - on the api route, to_json must be passed the nested resources

--- app/actions.rb
+++ app/actions.rb
@@ -11,3 +11,24 @@ end
 get '/organizations/new' do
   erb :signup
 end
+
+#################
+# ORGANIZATIONS #
+#################
+
+# list all organizations
+get '/organizations' do
+  erb :organizations 
+end
+
+# get all organizations
+get '/api/organizations' do 
+  content_type :json
+  Organization.all.to_json
+end
+
+# get organization by id
+get '/api/organizations/:id' do |id|
+  content_type :json
+  Organization.find(id).to_json
+end
