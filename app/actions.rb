c394baeab748359551123687bb420bf12a5d3a43
chertay <chertay@hotmail.ca>
2016-07-15 21:24:19 +0000

    basic templates finished

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,21 @@ end
 get '/styleguide' do
   erb :styleguide
 end
+
+
+get '/test1' do
+  erb :document
+end
+
+get '/test2' do
+  erb :business
+end
+get '/test3' do
+  erb :election
+end
+get '/test4' do
+  erb :meeting_details
+end
+get '/test5' do
+  erb :motion
+end
