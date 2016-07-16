d6339bb9a2867036f09f34e0c738989387d2e33c
Jen <jen@reiher.ca>
2016-07-16 21:54:47 +0000

    added basic navbar dropdown  structure

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+  // start of code for adding hover over pop up navbar
+
+  //  $(function() {
+  //    $('nav li ul').hide().removeClass('drop');
+  //      $('nav li').hover(function () {
+  //        $('ul', this).stop().slideDown(100);
+  //  },
+  //  function () {
+  //    $('ul', this).stop().slideUp(100);
+  //  }
+  // );
+  //  });
+
+  // end of code for adding hover over pop up navbar
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
