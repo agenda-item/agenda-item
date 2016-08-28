c2e4db47a54c6442121ebe901053d178a200f208
Monica Mow <mow.monica@gmail.com>
2016-08-27 23:57:41 -0700

    Add rack-flash gem and add login error message and redirect

--- app/actions.rb
+++ app/actions.rb
@@ -32,17 +32,17 @@ post '/login' do
   email = params[:email]
   password = params[:password]
 
-  #1. find user by username
+  #1. find user by email
   user = User.find_by(email: email)
   
-  #2. if that user exists and that user's password mathces the password input
-    if user.authenticate(password)
+  #2. if that user exists and that user's password matches the password input
+    if user && user.authenticate(password)
       #login
       session[:user_id] = user.id
-      redirect(to('/'))
+      redirect(to('/meetings'))
     else
-    @error_message = "Login failed."
-    erb(:login)
+    flash[:notice] = "Login failed. Please try again."
+    redirect '/login'
     end
 end
 
